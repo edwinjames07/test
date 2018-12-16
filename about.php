53d9c8769d9241e04499864a2e25ee55bbba4f19
edwin james <edwinjames07@gmail.com>
2018-12-16 15:21:30 +0530

    new files

---
+++ about.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="en">
+<head><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Home</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet"> 
+    <link href="css/lightbox.css" rel="stylesheet"> 
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+        <script src="js/html5shiv.js"></script>
+        <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+    <style>
+   th, td {
+    padding: 15px;
+} 
+
+</style>
+</head><!--/head-->
+
+<body>
+    <header id="header">      
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 overflow">
+                   <div class="social-icons pull-right">
+                        <ul class="nav nav-pills">
+                            <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                            <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                            <li><a href=""><i class="fa fa-google-plus"></i></a></li>
+                            <li><a href=""><i class="fa fa-dribbble"></i></a></li>
+                            <li><a href=""><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div> 
+                </div>
+             </div>
+        </div>
+        <div class="navbar navbar-inverse" role="banner">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <a class="navbar-brand" href="indexm.php">
+                        <h1><img src="images/logo.png" alt="logo"></h1>
+                    </a>
+                    
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="active"><a href="indexm.php">Home</a></li>  
+                        <li><a href="log.php">Login</a></li>  
+                         <li><a href="about.php">About</a></li>   
+
+                    </ul>
+                </div>
+               
+            </div>
+        </div>
+    </header>
+    <!--/#header-->
+ <div class="container">
+                    <h1 class="title-bar">ADI SHANKARA INSTITUTE OF ENGINEERING & TECHNOLOGY</h1>
+                    <div class="row">
+                        <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12">
+                            <div class="about-content">
+                                <p  font size="10" style="font-family:verdana;" align="justify" >Adi Shankara Institute of Engineering & Technology was established at Kalady with the aim of providing value added technical education with a flair of professional excellence and ethical values.The college is run by ADI SANKARA TRUST, a registered trust which has carved a niche in the educational sector by running Sree Sankara College, Sree Sarada School, Adi Sankara Training College, Sree Sarada Special School, DDU Kaushal Kendra all at Kalady.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
+
+                                    <div >
+                                       <img src="images/clg.jpg" class="slider-hill" height="250" width="650"alt="slider image">
+                                    </div>    
+
+                        </div>
+                    </div>
+                </div>
+  <div style="background-color:#DCDCDC;padding:75px;" align="center">
+<table>
+    <col width="550">
+  <col width="550">
+<tr>
+    <th colspan="1" align="center">OUR VISION<hr></th>
+     <th  colspan="1" align="center">OUR MISSION<hr></th>
+</tr> 
+<tr>
+    <th font size="10" style="font-family:verdana;">To emerge as a Center of Excellence in Engineering, Technology and Management by imparting quality education,Focussing on empowerment and innovation.</th>
+    <th font size="10" style="font-family:verdana;">  Impart quality professional education for total upliftment of the society.<br>Mould competent professionals who are socially committed and responsible citizens<br> Create congenial academic ambience that kindles innovative thinking and research.</th>
+</tr>         
+ </table>
+</div>
+<div align="center">
+<table>
+    <caption><h1 align="center">OUR MENTORS</h1></caption>
+    <tr>
+        <th><img src="images/princi.jpg" alt="Fjords" width="200" height="200"></th>
+        <th><img src="images/hod.jpg" alt="Fjords" width="200" height="200"></th>
+        <th><img src="images/mural.jpg" alt="Fjords" width="200" height="200"></th>
+        <!--<th><img src="images/basil.jpg" alt="Fjords" width="200" height="200"></th>-->
+        <th><img src="images/gripsy.jpg" alt="Fjords" width="200" height="200"></th>
+        <th><img src="images/vidya.jpg" alt="Fjords" width="200" height="200"></th>
+        <th><img src="images/neetha.jpg" alt="Fjords" width="200" height="200"></th>
+        <th><img src="images/divya.jpg" alt="Fjords" width="200" height="200"></th>
+
+    </tr>
+    <tr>
+        <th>Dr.Neelakantan P C<br>PRINCIPAL</th>
+        <th>Dr.M.A.Dorairangaswamy<br>HOD</th>
+        <th>Dr. Murali P<br>Associate Professor</th>
+        <!--<th>Basil C. Sunny<br>Assistant Professor</th>-->
+        <th>Gripsy Paul <br>Assistant Professor</th>
+        <th>Vidya Gopinath<br>Assistant Professor</th>
+        <th>Neetha K Nataraj<br>Assistant Professor</th>
+        <th>Divya D<br>Assistant Professor</th>
+    </tr>
+</table>
+</div>   
+<br>
+<br>
+<div style="background-color:#DCDCDC;padding:50px;" align="center">
+<table>
+    <caption ><h1 align="center">OUR TEAM</h1></caption>
+    <tr>
+        <th><img src="images/arya.jpg"" alt="Fjords" width="250" height="200"></th>
+        <th><img src="images/edwin1.jpg" alt="Fjords" width="250" height="200"></th>
+        <th><img src="images/rinta.jpg" alt="Fjords" width="250" height="200"></th>
+        <th><img src="images/robin1.jpg" alt="Fjords" width="250" height="200"></th>
+     
+        
+    </tr>
+    <tr>
+        <th>Arya K S<br>CS Dept.</th>
+        <th>Edwin James<br>CS Dept.</th>
+        <th>Rinta Antony<br>CS Dept.</th>
+        <th>Robin Thomas<br>CS Dept.</th>
+     
+        
+    </tr>
+</table>
+</div>   
+    <footer id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 text-center bottom-separator">
+                    <img src="images/home/under.png" class="img-responsive inline" alt="">
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <div class="testimonial bottom" style="width:300px;height:500px;">
+                        <h2>Get the Best From Us</h2>
+                        <div class="media">
+                            <div class="pull-left">
+
+                            </div>
+                            <div class="media-body">
+                                <blockquote>Fell free to contact us for any assistance.</blockquote>
+                                <h3><a href="#">- 9605403492</a></h3>
+                            </div>
+                         </div>
+                        <div class="media">
+                          <!--  <div class="pull-left">
+
+                            </div>-->
+                            <div class="media-body">
+                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="contact-info bottom">
+                        <h2>Contacts</h2>
+                        <address>
+                        E-mail: <a href="mailto:edwinjames07@gmail.com">edwinjames07@gmail.com</a> <br>
+                        Phone: +91 9605403492<br>
+                        </address>
+
+                        <h2>Address</h2>
+                        <address>
+                        ASIET <br>
+                        CS S7, <br>
+                        Kalady <br>
+                        India <br>
+                        </address>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-12">
+                    <div class="contact-form bottom">
+                        <h2>Send a message</h2>
+                        <form id="main-contact-form" name="contact-form" method="post" action="sendemail.php">
+                            <div class="form-group">
+                                <input type="text" name="name" class="form-control" required="required" placeholder="Name">
+                            </div>
+                            <div class="form-group">
+                                <input type="email" name="email" class="form-control" required="required" placeholder="Email Id">
+                            </div>
+                            <div class="form-group">
+                                <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Your text here"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" name="submit" class="btn btn-submit" value="Submit">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="copyright-text text-center">
+                        <p>&copy; RARE Creations 2017. All Rights Reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--/#footer-->
+
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/lightbox.min.js"></script>
+    <script type="text/javascript" src="js/wow.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>   
+</body>
+</html>
+
+    
